942504e530074e807243b71a38e00300dc43f153
arielgindi <45123820+arielgindi@users.noreply.github.com>
2024-12-19 17:08:32 +0200

    refactor: remove final_preprocessing as it is unnecessary

--- calculator/expression_calculator.py
+++ calculator/expression_calculator.py
@@ -3,7 +3,7 @@ from calculator.tokenize import tokenize
 from calculator.Token import Token
 from calculator.postfix_parser import convert_to_postfix
 from calculator.postfix_calculator import postfix_calculator
-from calculator.final_preprocessing import final_preprocessing
+from calculator.utils.remove_spaces import remove_spaces
 from calculator.validate_tilda import validate_tilda
 
 
@@ -11,8 +11,9 @@ def calculate_expression(expression: str) -> float | int:
     if not expression:
         raise ValueError("Expression cannot be empty.")
 
-    normalized_expr = final_preprocessing(expression)
-    tokens: list[Token] = tokenize(normalized_expr)
+    no_space_expr = remove_spaces(expression)
+
+    tokens: list[Token] = tokenize(no_space_expr)
 
     unary_tokens = normalize_unary(tokens)
     validate_tilda(unary_tokens)
